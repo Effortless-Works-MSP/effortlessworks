6c085d9de0a0da1adaa332bd27b5704218998a16
Elif Çakmak <elifcakmak.blog@gmail.com>
2026-02-23 20:32:30 -0500

    biz nav

--- app/000000/1-1-0-biz-nav/navigation.tsx
+++ app/000000/1-1-0-biz-nav/navigation.tsx
@@ -17,57 +17,56 @@ const Navigation = () => {
 
   return (
     <>
-    <header className={`mainheader ${scrolled ? "shrink" : ""}`}>
-      {/* Blog logo */}
-      <a href="https://www.elifcakmak.blog/" className="logo-link">
-        <Image
-          src="/elif-logo/elifcakmaklogo.svg"
-          alt="Elif Çakmak Logo"
-          width={100}
-          height={100}
-          className="logo"
-        />
-      </a>
+      <header className={`mainheader ${scrolled ? "shrink" : ""}`}>
+        {/* Blog logo */}
+        <a href="https://www.elifcakmak.blog/" className="logo-link">
+          <Image
+            src="/elif-logo/elifcakmaklogo.svg"
+            alt="Elif Çakmak Logo"
+            width={100}
+            height={100}
+            className="logo"
+          />
+        </a>
 
-      {/* Main Home logo */}
-      <Link href="/" className="logo-link">
-        <Image
-          src="/elif-logo/effortlessworksdark.svg"
-          alt="Effortless Works"
-          width={100}
-          height={100}
-          className="logo2"
-        />
-      </Link>
-
-      {/* Navigation */}
-      <nav className="mainnav">
-        {/* Goes to whole site home */}
-        <Link href="/" className="Home-button">
-          Main Home
+        {/* Main Home logo */}
+        <Link href="/" className="logo-link">
+          <Image
+            src="/elif-logo/effortlessworksdark.svg"
+            alt="Effortless Works"
+            width={100}
+            height={100}
+            className="logo2"
+          />
         </Link>
 
-        {/* Goes to business home page */}
-        <Link href="/000002/business" className="mainnav-link">
-          Business 🏢
-        </Link>
+        {/* Navigation */}
+        <nav className="mainnav">
+          <Link href="/" className="Home-button">
+            Main Home
+          </Link>
 
-        {/* Scroll buttons (use query params to navigate + scroll) */}
-        <Link href="/000002/business?scroll=backoffice" className="newsletter-button">
-          Back Office
-        </Link>
+          <Link href="/000002/business" className="mainnav-link">
+            Business 🏢
+          </Link>
 
-        <Link href="/000002/business?scroll=projectmanagement" className="newsletter-button">
-          Project Management
-        </Link>
+          {/* ✅ Updated anchor links */}
+          <Link href="/000002/business#backoffice" className="newsletter-button">
+            Back Office
+          </Link>
 
-        <Link href="/000002/business?scroll=howtos" className="newsletter-button">
-          How Tos
-        </Link>
-      </nav>
-    </header>
-    {/* Spacer div pushes content below fixed header */}
-      <div style={{ height: '210px' }} />
+          <Link href="/000002/business#projectmanagement" className="newsletter-button">
+            Project Management
+          </Link>
+
+          <Link href="/000002/business#howtos" className="newsletter-button">
+            How Tos
+          </Link>
+        </nav>
+      </header>
+
+      {/* Spacer for fixed header */}
+      <div style={{ height: "210px" }} />
     </>
   );
 };
--- app/000002/business/page.tsx
+++ app/000002/business/page.tsx
@@ -1,30 +1,11 @@
-"use client";
-
-import { useEffect } from "react";
-import { useSearchParams } from "next/navigation";
-import CustomCursor from '../../000000/0-0-cursor/page';
-import Navigation from '../../000000/1-1-0-biz-nav/navigation';
-import Footer from '../../000000/0-2-footer/footer'
-import Backoffice from '../../000003/b-home-backoffice/home-backoffice';
-import ProjectManagement from '../../000003/b-home-projectmanagement/home-projectmanagement';
-import HowTos from '../../000003/b-home-howtos/home-howtos';
+import CustomCursor from "../../000000/0-0-cursor/page";
+import Navigation from "../../000000/1-1-0-biz-nav/navigation";
+import Footer from "../../000000/0-2-footer/footer";
+import Backoffice from "../../000003/b-home-backoffice/home-backoffice";
+import ProjectManagement from "../../000003/b-home-projectmanagement/home-projectmanagement";
+import HowTos from "../../000003/b-home-howtos/home-howtos";
 
 export default function BusinessHome() {
-  const searchParams = useSearchParams();
-  const scrollTo = searchParams.get("scroll");
-
-  useEffect(() => {
-    if (scrollTo) {
-      // Wait a tick to ensure sections are rendered
-      setTimeout(() => {
-        const element = document.getElementById(scrollTo);
-        if (element) {
-          element.scrollIntoView({ behavior: "smooth" });
-        }
-      }, 50);
-    }
-  }, [scrollTo]);
-
   return (
     <div className="home-container">
       <CustomCursor />
